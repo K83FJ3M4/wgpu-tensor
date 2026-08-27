4859e827fe4458e16a94ab595b5bfbc0d05bee45
Phillip Mendel <phillip.mendel@posteo.de>
2026-08-27 13:16:09 +0200

    implemented unary backwards definitions

--- src/pipelines/unary.rs
+++ src/pipelines/unary.rs
@@ -1,6 +1,7 @@
 use bytemuck::{Pod, Zeroable};
 use wgpu::{ComputePipeline, ComputePipelineDescriptor, PipelineLayoutDescriptor, include_wgsl};
 
+use crate::optimizers::AutogradEncoder;
 use crate::pipelines::Pipelines;
 use crate::{Tensor, TensorEncoder, TensorError};
 
@@ -24,6 +25,7 @@ impl UnaryOperation {
     pub const EXPONENTIAL: Self = Self(5);
     pub const LOGARITHM: Self = Self(6);
     pub const COPY: Self = Self(7);
+    pub const SIGN: Self = Self(8);
 }
 
 impl<'scope> TensorEncoder<'scope> {
@@ -31,80 +33,206 @@ impl<'scope> TensorEncoder<'scope> {
         &mut self,
         operand: &Tensor<'scope>,
     ) -> Result<Tensor<'scope>, TensorError> {
-        self.unary(
+        let res = self.unary(
             UnaryOperation::NEGATE,
             operand,
-        )
-    } 
+        )?;
+
+        if let Some(autograd) = self.autograd.as_mut() {
+            autograd.backwards_unary(
+                operand,
+                &res,
+                |encoder, output_grad| encoder.negate(output_grad)
+            );
+        }
+
+        Ok(res)
+    }
 
     pub fn absolute(
         &mut self,
         operand: &Tensor<'scope>,
     ) -> Result<Tensor<'scope>, TensorError> {
-        self.unary(
+        let res = self.unary(
             UnaryOperation::ABSOLUTE,
             operand,
-        )
+        )?;
+
+        if let Some(autograd) = self.autograd.as_mut() {
+            let operand_value = operand.clone();
+            autograd.backwards_unary(
+                operand,
+                &res,
+                move |encoder, output_grad| {
+                    let sign = encoder.sign(&operand_value)?;
+                    encoder.multiply(output_grad, &sign)
+                }
+            );
+        }
+
+        Ok(res)
     }
 
     pub fn reciprocal(
         &mut self,
         operand: &Tensor<'scope>,
     ) -> Result<Tensor<'scope>, TensorError> {
-        self.unary(
+        let res = self.unary(
             UnaryOperation::RECIPROCAL,
             operand,
-        )
+        )?;
+
+        if let Some(autograd) = self.autograd.as_mut() {
+            let result = res.clone();
+            autograd.backwards_unary(
+                operand,
+                &res,
+                move |encoder, output_grad| {
+                    let square = encoder.multiply(&result, &result)?;
+                    let gradient = encoder.multiply(output_grad, &square)?;
+                    encoder.negate(&gradient)
+                }
+            );
+        }
+
+        Ok(res)
     }
 
     pub fn square_root(
         &mut self,
         operand: &Tensor<'scope>,
     ) -> Result<Tensor<'scope>, TensorError> {
-        self.unary(
+        let res = self.unary(
             UnaryOperation::SQUARE_ROOT,
             operand,
-        )
+        )?;
+
+        if let Some(autograd) = self.autograd.as_mut() {
+            let result = res.clone();
+            autograd.backwards_unary(
+                operand,
+                &res,
+                move |encoder, output_grad| {
+                    let denominator = encoder.add(&result, &result)?;
+                    encoder.divide(output_grad, &denominator)
+                }
+            );
+        }
+
+        Ok(res)
     }
 
     pub fn reciprocal_square_root(
         &mut self,
         operand: &Tensor<'scope>,
     ) -> Result<Tensor<'scope>, TensorError> {
-        self.unary(
+        let res = self.unary(
             UnaryOperation::RECIPROCAL_SQUARE_ROOT,
             operand,
-        )
+        )?;
+
+        if let Some(autograd) = self.autograd.as_mut() {
+            let result = res.clone();
+            autograd.backwards_unary(
+                operand,
+                &res,
+                move |encoder, output_grad| {
+                    let square = encoder.multiply(&result, &result)?;
+                    let cube = encoder.multiply(&square, &result)?;
+                    let gradient = encoder.multiply(output_grad, &cube)?;
+                    let one = encoder.ones(1)?;
+                    let two = encoder.add(&one, &one)?;
+                    let gradient = encoder.divide(&gradient, &two)?;
+                    encoder.negate(&gradient)
+                }
+            );
+        }
+
+        Ok(res)
     }
 
     pub fn exponential(
         &mut self,
         operand: &Tensor<'scope>,
     ) -> Result<Tensor<'scope>, TensorError> {
-        self.unary(
+        let res = self.unary(
             UnaryOperation::EXPONENTIAL,
             operand,
-        )
+        )?;
+
+        if let Some(autograd) = self.autograd.as_mut() {
+            let result = res.clone();
+            autograd.backwards_unary(
+                operand,
+                &res,
+                move |encoder, output_grad| {
+                    encoder.multiply(output_grad, &result)
+                }
+            );
+        }
+
+        Ok(res)
     }
 
     pub fn logarithm(
         &mut self,
         operand: &Tensor<'scope>,
     ) -> Result<Tensor<'scope>, TensorError> {
-        self.unary(
+        let res = self.unary(
             UnaryOperation::LOGARITHM,
             operand,
-        )
+        )?;
+
+        if let Some(autograd) = self.autograd.as_mut() {
+            let operand_value = operand.clone();
+            autograd.backwards_unary(
+                operand,
+                &res,
+                move |encoder, output_grad| {
+                    encoder.divide(output_grad, &operand_value)
+                }
+            );
+        }
+
+        Ok(res)
     }
 
     pub fn copy(
         &mut self,
         operand: &Tensor<'scope>,
     ) -> Result<Tensor<'scope>, TensorError> {
-        self.unary(
+        let res = self.unary(
             UnaryOperation::COPY,
             operand,
-        )
+        )?;
+
+        if let Some(autograd) = self.autograd.as_mut() {
+            autograd.backwards_unary(
+                operand,
+                &res,
+                |_, output_grad| Ok(output_grad.clone())
+            );
+        }
+
+        Ok(res)
+    }
+
+    pub fn sign(
+        &mut self,
+        operand: &Tensor<'scope>,
+    ) -> Result<Tensor<'scope>, TensorError> {
+        let res = self.unary(UnaryOperation::SIGN, operand)?;
+
+        if let Some(autograd) = self.autograd.as_mut() {
+            let operand_shape = operand.shape();
+            autograd.backwards_unary(
+                operand,
+                &res,
+                move |encoder, _| encoder.zeros(operand_shape)
+            );
+        }
+
+        Ok(res)
     }
 
     fn unary(
@@ -147,6 +275,36 @@ impl<'scope> TensorEncoder<'scope> {
         }
 
         Ok(output)
+    }
+}
+
+impl<'scope> AutogradEncoder<'scope> {
+    fn backwards_unary(
+        &mut self,
+        operand: &Tensor<'scope>,
+        res: &Tensor<'scope>,
+        gradient: impl FnOnce(
+            &mut TensorEncoder<'scope>,
+            &Tensor<'scope>,
+        ) -> Result<Tensor<'scope>, TensorError> + 'scope,
+    ) {
+        if self.require([res], operand) {
+            let res_weak = res.downgrade();
+            let operand_weak = operand.downgrade();
+
+            self.backwards(move |encoder, gradients| {
+                let Some(output_grad) = gradients.remove(res_weak) else {
+                    return Ok(())
+                };
+
+                let input_grad = gradient(encoder, &output_grad)?;
+                gradients.insert(
+                    encoder,
+                    operand_weak,
+                    input_grad
+                )
+            });
+        }
     }
 }
 
